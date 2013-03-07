d6aaa4b0cfd09ce49d45ef904536ddfe3902d8b8
Yang Hua <htmlcook@gmail.com>
2013-03-07 10:31:01 +0800

    add file

--- admin/__init__.py
+++ admin/__init__.py
@@ -2,7 +2,10 @@ import web
 import config
 
 from app.helpers import tags, utils
+from app.models import setting
 
+setting.update_view_site_setting()
 view_globals = utils.get_module_functions(tags)
+view_globals.update(config.view_globals)
 
 render = web.template.render('admin/templates', cache=config.cache, base='layout', globals = view_globals)
